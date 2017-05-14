c7bb84bc144cc0b9d5b25890bc57884e9ae32be2
Alexey Pobery <poberyaa@google.com>
2017-05-15 00:31:38 +0300

    Add open dialog

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,9 +1,10 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
-
 #include "circlerecognizer.h"
 
 #include <QPixmap>
+#include <QFileDialog>
+
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -58,7 +59,8 @@ void MainWindow::DrawCircle(QImage& image, const Circle& circle)
 
 void MainWindow::on_actionOpen_triggered()
 {
-    QPixmap image("./1.jpg");
+    QString fileName = QFileDialog::getOpenFileName(this, tr("Open Image"), ".", tr("Image Files (*.png *.jpg *.bmp)"));
+    QPixmap image(fileName);
     image = image.scaled(ui->label->width(), ui->label->height(), Qt::KeepAspectRatio);
 
     QImage original = image.toImage();
